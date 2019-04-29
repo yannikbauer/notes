1883ac853668ec2fa36f80118fd804c9b476b989
Martin Spacek <git@mspacek.mm.st>
2019-04-30 00:22:31 +0200

    Update 00_intro

--- 00_intro/LFP_analysis_example.py
+++ 00_intro/LFP_analysis_example.py
@@ -3,22 +3,24 @@ import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 # load data:
-data = np.load('LFP_example_data.npy')
-npoints = len(data)
+mV = np.load('LFP_example_data.npy') # voltage signal, in mV
+V = mV / 1000 # convert to V
+npoints = len(V)
 sampfreq = 1000 # Hz
 t = np.arange(npoints) / sampfreq
 
 # plot all data:
 f1, a1 = plt.subplots(figsize=(10, 3)) # create a figure and axes
-a1.plot(t, data, '-', c='black')
+a1.plot(t, V, '-', c='black')
+a1.set_xlim((t[0], t[-1])) # set hard x limits
 a1.set_title('LFP example data')
 a1.set_xlabel('Time (s)')
-a1.set_ylabel('Volage (mV)')
+a1.set_ylabel('Volage (V)')
 
 # calculate spectrogram using Welch's periodogram method:
 winwidth = 5 # window width, seconds
 winwidthsamples = int(winwidth * sampfreq)
-P, freqs, t = mpl.mlab.specgram(data, winwidthsamples, sampfreq)
+P, freqs, t = mpl.mlab.specgram(V, winwidthsamples, sampfreq)
 
 # set frequency limits:
 fmin, fmax = 0, 50 # set frequency limits, in Hz
